3bfe0d6c7943a9ae68e75d5b5e5d28b5fc4a7938
druid <angular.druid@gmail.com>
2023-03-15 01:57:23 -0400

    Initial commit of wasm cloud template for tinygo project.

---
+++ cosmonic_hedge.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/wasmcloud/actor-tinygo"
+	"github.com/wasmcloud/interfaces/httpserver/tinygo"
+)
+
+func main() {
+	me := CosmonicHedge{}
+	actor.RegisterHandlers(httpserver.HttpServerHandler(&me))
+}
+
+type CosmonicHedge struct{}
+
+func (e *CosmonicHedge) HandleRequest(ctx *actor.Context, req httpserver.HttpRequest) (*httpserver.HttpResponse, error) {
+	r := httpserver.HttpResponse{
+		StatusCode: 200,
+		Header:     make(httpserver.HeaderMap, 0),
+		Body:       []byte("hello"),
+	}
+	return &r, nil
+}
